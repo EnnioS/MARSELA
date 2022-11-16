737567cfdd172807ee635e8049e7d63f75a54b99
EnnioS <ennio.saenz.ext@zed.com>
2022-11-15 22:42:02 -0600

    agregando clase Conexion a MySQL DB

--- src/main/java/com/gmc/marsela/entity/Estudio.java
+++ src/main/java/com/gmc/marsela/entity/Estudio.java
@@ -1,136 +1,196 @@
 package com.gmc.marsela.entity;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.DatePicker;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Estudio {
 
-    private DatePicker fecha;
-    private String ciudad;
-    private String sitio;
-    private String observaciones;
 
-    private Double area;
-    private String uMedida;
-    private Integer cantDepoBuenEstado;
-    private Integer cantDepoMalEstado;
-    private Double cod;
-    private Integer nivelContaminacion;
-    private String resultado;
+    private ObjectProperty<Date> fecha;
+    private StringProperty ciudad;
+    private StringProperty sitio;
+    private StringProperty observaciones;
+
+    private DoubleProperty area;
+    private StringProperty uMedida;
+    private IntegerProperty cantDepoBuenEstado;
+    private IntegerProperty cantDepoMalEstado;
+    private DoubleProperty cod;
+    private IntegerProperty nivelContaminacion;
+    private StringProperty resultado;
 
 
-    public String getSugerencia() {
-        return sugerencia;
+    public Date getFecha() {
+        return fecha.get();
     }
 
-    public void setSugerencia(String sugerencia) {
-        this.sugerencia = sugerencia;
-    }
-
-    private String sugerencia;
-
-    public DatePicker getFecha() {
+    public ObjectProperty<Date> fechaProperty() {
         return fecha;
     }
 
-    public void setFecha(DatePicker fecha) {
-        this.fecha = fecha;
+    public void setFecha(Date fecha) {
+        this.fecha.set(fecha);
     }
 
+
+
     public String getCiudad() {
+        return ciudad.get();
+    }
+
+    public StringProperty ciudadProperty() {
         return ciudad;
     }
 
     public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
+        this.ciudad.set(ciudad);
     }
 
     public String getSitio() {
+        return sitio.get();
+    }
+
+    public StringProperty sitioProperty() {
         return sitio;
     }
 
     public void setSitio(String sitio) {
-        this.sitio = sitio;
+        this.sitio.set(sitio);
     }
 
     public String getObservaciones() {
+        return observaciones.get();
+    }
+
+    public StringProperty observacionesProperty() {
         return observaciones;
     }
 
     public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
+        this.observaciones.set(observaciones);
     }
 
-    public Double getArea() {
+    public double getArea() {
+        return area.get();
+    }
+
+    public DoubleProperty areaProperty() {
         return area;
     }
 
-    public void setArea(Double area) {
-        this.area = area;
+    public void setArea(double area) {
+        this.area.set(area);
     }
 
     public String getuMedida() {
+        return uMedida.get();
+    }
+
+    public StringProperty uMedidaProperty() {
         return uMedida;
     }
 
     public void setuMedida(String uMedida) {
-        this.uMedida = uMedida;
+        this.uMedida.set(uMedida);
     }
 
-    public Integer getCantDepoBuenEstado() {
+    public int getCantDepoBuenEstado() {
+        return cantDepoBuenEstado.get();
+    }
+
+    public IntegerProperty cantDepoBuenEstadoProperty() {
         return cantDepoBuenEstado;
     }
 
-    public void setCantDepoBuenEstado(Integer cantDepoBuenEstado) {
-        this.cantDepoBuenEstado = cantDepoBuenEstado;
+    public void setCantDepoBuenEstado(int cantDepoBuenEstado) {
+        this.cantDepoBuenEstado.set(cantDepoBuenEstado);
     }
 
-    public Integer getCantDepoMalEstado() {
+    public int getCantDepoMalEstado() {
+        return cantDepoMalEstado.get();
+    }
+
+    public IntegerProperty cantDepoMalEstadoProperty() {
         return cantDepoMalEstado;
     }
 
-    public void setCantDepoMalEstado(Integer cantDepoMalEstado) {
-        this.cantDepoMalEstado = cantDepoMalEstado;
+    public void setCantDepoMalEstado(int cantDepoMalEstado) {
+        this.cantDepoMalEstado.set(cantDepoMalEstado);
     }
 
-    public Double getCod() {
+    public double getCod() {
+        return cod.get();
+    }
+
+    public DoubleProperty codProperty() {
         return cod;
     }
 
-    public void setCod(Double cod) {
-        this.cod = cod;
+    public void setCod(double cod) {
+        this.cod.set(cod);
     }
 
-    public Integer getNivelContaminacion() {
+    public int getNivelContaminacion() {
+        return nivelContaminacion.get();
+    }
+
+    public IntegerProperty nivelContaminacionProperty() {
         return nivelContaminacion;
     }
 
-    public void setNivelContaminacion(Integer nivelContaminacion) {
-        this.nivelContaminacion = nivelContaminacion;
+    public void setNivelContaminacion(int nivelContaminacion) {
+        this.nivelContaminacion.set(nivelContaminacion);
     }
 
     public String getResultado() {
+        return resultado.get();
+    }
+
+    public StringProperty resultadoProperty() {
         return resultado;
     }
 
     public void setResultado(String resultado) {
-        this.resultado = resultado;
+        this.resultado.set(resultado);
     }
 
     @Override
     public String toString() {
         return "Estudio{" +
                 "fecha=" + fecha +
-                ", ciudad='" + ciudad + '\'' +
-                ", sitio='" + sitio + '\'' +
-                ", observaciones='" + observaciones + '\'' +
+                ", ciudad=" + ciudad +
+                ", sitio=" + sitio +
+                ", observaciones=" + observaciones +
                 ", area=" + area +
-                ", uMedida='" + uMedida + '\'' +
+                ", uMedida=" + uMedida +
                 ", cantDepoBuenEstado=" + cantDepoBuenEstado +
                 ", cantDepoMalEstado=" + cantDepoMalEstado +
                 ", cod=" + cod +
                 ", nivelContaminacion=" + nivelContaminacion +
-                ", resultado='" + resultado + '\'' +
-                ", sugerencia='" + sugerencia + '\'' +
+                ", resultado=" + resultado +
                 '}';
+    }
+
+    public void saveStudy(){
+
+    }
+    public void updateStudy(){
+
+    }
+    public void deleteStudy(){
+
+    }
+    public ArrayList<Estudio> searchAllStudy(Integer id){
+        return null;
+    }
+    public List<Estudio> searchStudy(){
+        return null;
     }
 }
---
+++ src/main/java/com/gmc/marsela/util/DbConnection.java
@@ -0,0 +1,35 @@
+package com.gmc.marsela.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    public Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void establecerConnection(){
+        String databaseName = "marsela_db_ices_2022";
+        String databaseUser = "root";
+        String databasePassword = "123456";
+        String url ="jdbc:mysql://localhost/"+databaseName;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, databaseUser, databasePassword);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        connection.close();
+    }
+}
